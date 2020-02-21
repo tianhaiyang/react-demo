08f55fded8888555560fde7e370963cb413d6ad8
Ocean <ocean@tradexport.com>
2020-02-21 14:46:57 +0800

    修改antd使用代码

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      <Button type="primary"></Button>
+      <Button type="primary">Button</Button>
       </header>
     </div>
   );
