0c18a64e6d2ed1a45df0d43ec9f5cc5be1ef545f
Ocean <ocean@tradexport.com>
2020-02-21 14:17:14 +0800

    Updates

---
+++ precache-manifest.6d2ed65028a5be7f00bb1d0d6fe93831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10ee53969e2c3e59f25b84a3e9d6636",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "acec05465ad8980b08b9",
+    "url": "/react-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f828f6475f3dadbf4a81",
+    "url": "/react-demo/static/js/2.fe49ad7d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-demo/static/js/2.fe49ad7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acec05465ad8980b08b9",
+    "url": "/react-demo/static/js/main.4900c65a.chunk.js"
+  },
+  {
+    "revision": "47e753723fbfef15621a",
+    "url": "/react-demo/static/js/runtime-main.0f615a0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
